f10705e68818cc36e78a241618b4e5d328dacdce
marcacohen <marcacohen@gmail.com>
2023-03-30 18:42:07 +0100

    add env.sh

---
+++ scripts/env.sh
@@ -0,0 +1,6 @@
+export APP=quizrd
+export REGION=us-central1
+export PROD_PROJECT=$APP-prod-382117
+export STAGE_PROJECT=$APP-stage-382117
+export OPS_PROJECT=$APP-ops-382117
+export SKIP_TRIGGERS=1
