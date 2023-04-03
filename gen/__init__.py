553f2760d749fb8ce9580eb4802c09ec3565f2db
marcacohen <marcacohen@gmail.com>
2023-04-03 22:42:31 +0100

    tweak generator

--- gen/__init__.py
+++ gen/__init__.py
@@ -7,15 +7,17 @@ supported_gens = {
     "chatgpt":  None,
 }
 
-for i in supported_gens:
-    supported_gens[i] = importlib.import_module("gen." + i)
-
 class Generator:
     def __init__(self, type):
         if type not in supported_gens:
             raise Exception(f"Unsupported generator type {type}.")
+        if not supported_gens[type]:
+            supported_gens[type] = importlib.import_module("gen." + type)
         self.type = type
         self._gen = supported_gens[type].Generator()
+
+    def get_gens(self):
+        return supported_gens.keys()
 
     def __str__(self):
         return self._gen.__str__()
