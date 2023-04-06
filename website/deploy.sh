2d8d1cd94bff99760958115c2b90acd99df637dc
marcacohen <marcacohen@gmail.com>
2023-04-06 19:44:32 +0100

    fix bucket name

--- website/deploy.sh
+++ website/deploy.sh
@@ -1,4 +1,4 @@
 . ../scripts/env.sh
 gcloud builds submit . --tag=gcr.io/$PROJECT_ID/website
 gcloud run deploy website --region $REGION --image=gcr.io/$PROJECT_ID/website \
-  --set-env-vars "$SITE_VARS" website
+  --set-env-vars "$SITE_VARS"
