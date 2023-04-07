016fa857d6386f6bd6b1b635856365223d16211b
marcacohen <marcacohen@gmail.com>
2023-04-07 14:01:56 +0100

    debugging log

--- website/middleware/auth.py
+++ website/middleware/auth.py
@@ -149,6 +149,8 @@ def get_refreshed_token(refresh_token):
         return None, None
 
     id_token = updates.get("id_token")
+    log(f"updates: {updates}")
+
     expiration = datetime.timestamp(datetime.now()) + updates.get("expires_in")
 
     return id_token, expiration
