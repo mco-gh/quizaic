3d93923e0ed2c4d4b81ba0c7a246e824cf91a70a
marcacohen <marcacohen@gmail.com>
2023-04-08 23:54:32 +0100

    add host view and template

---
+++ website/views/host.py
@@ -0,0 +1,35 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from flask import Blueprint, g, redirect, request, render_template
+from middleware.logging import log
+from views.helpers.time import convert_utc
+
+host_bp = Blueprint("play", __name__, template_folder="templates")
+
+
+@host_bp.route("/host/<int:pin>")
+def host(pin):
+    current_user = None
+    log(f"pin: {pin}")
+
+    if g.session_data:
+        current_user = g.session_data.get("email")
+
+    try:
+        quizzes = g.api.quizzes_get()
+    except Exception as e:
+        log(f"Exception when listing quizzes view: {e}", severity="ERROR")
+        quizzes = []
+    return render_template("host.html", pin=pin, quizzes=quizzes, current_user=current_user)
