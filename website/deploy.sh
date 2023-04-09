3f3f404259612d00484e6d10bdee1f901d76b67e
marcacohen <marcacohen@gmail.com>
2023-04-09 15:58:40 +0100

    fix link highlighting

--- website/deploy.sh
+++ website/deploy.sh
@@ -1,4 +1,6 @@
 . ../scripts/env.sh
+export PROJECT_ID=$STAGE_PROJECT
+gcloud config set project $PROJECT_ID
 gcloud builds submit . --tag=gcr.io/$PROJECT_ID/website
 gcloud run deploy website --region $REGION --image=gcr.io/$PROJECT_ID/website \
   --set-env-vars "$SITE_VARS"
