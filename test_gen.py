cc667d8f3b83ca7201264baaf0b7d54e95f09b05
marcacohen <marcacohen@gmail.com>
2023-05-19 13:21:33 -0400

    format fix to test_gen.py

--- test_gen.py
+++ test_gen.py
@@ -1,6 +1,6 @@
 import gen
+import json
 g = gen.Generator("jeopardy")
 topics = g.get_topics()
-print(topics)
 quiz = g.gen_quiz("Quotations", 3, 4)
-print(quiz)
+print(json.dumps(json.loads(quiz), indent=4))
