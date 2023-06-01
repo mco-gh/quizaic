c3e398e89f88c9538a7a7843748b8922a5dbcf50
marcacohen <marcacohen@gmail.com>
2023-05-31 23:09:56 -0400

    first attempt at palm's gen_quiz method

--- website/gen/palm/__init__.py
+++ website/gen/palm/__init__.py
@@ -16,15 +16,34 @@ class Generator:
         return ["freeform", "multiple-choice"]
 
     def gen_quiz(self, topic, numQuestions, numAnswers):
-        return '''[   
-                    { 
-                      "question":  "palm question",
-                      "correct":   "palm answer",
-                      "responses": [   
-                                     "palm answer 1",
-                                     "palm answer 2",
-                                     "palm answer 3",
-                                     "palm answer 4"
-                                   ]
-                    }
-                  ]'''
+        import pprint
+        import google.generativeai as palm
+        palm.configure(api_key="AIzaSyB_3DiddpC5Y53jHD3Sc_E8EWCgKwUeyNk")
+        models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+        model = models[0].name
+
+        prompt = """
+Generate a trivia quiz about popular music. I'd like 10 questions, each with four multiple choice answers. One answer to each question may be funny or silly. Please format the quiz in json document like this, with no internal single quotes or escaped double quotes and no line breaks:
+
+[
+          {
+              "question": "Which band sold the most records of all time?",
+              "correct": "The Beatles",
+              "responses": [
+                  "The Rolling Stones",
+                  "The Beatles",
+                  "Oasis",
+                  "Spinal Tap"
+              ]
+          },
+          ...
+]
+"""
+        completion = palm.generate_text(
+            model=model,
+            prompt=prompt,
+            temperature=0,
+            max_output_tokens=800,
+        )
+
+        return completion.result
