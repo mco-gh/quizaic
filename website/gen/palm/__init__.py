3c039b659240f1eafb5e3f5d117243dadcf86e05
marcacohen <marcacohen@gmail.com>
2023-06-01 10:39:47 -0400

    change default num questions to 5 and fix palm prompt

--- website/gen/palm/__init__.py
+++ website/gen/palm/__init__.py
@@ -22,9 +22,22 @@ class Generator:
         models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
         model = models[0].name
 
-        prompt = f'''
-Generate a trivia quiz about {topic} represented in json as an array of {num_questions} object, where each object contains a question string associated with key "question", a correct answer string associated with key "correct", and an array of {num_answers} multiple choice answers associated with key "responses".
-        '''
+        prompt = f"""
+Generate a trivia quiz about {topic} represented in json as an array of {num_questions} object, where each object contains a question string associated with key "question", a correct answer string associated with key "correct", and an array of {num_answers} multiple choice answers associated with key "responses". I'd like {num_questions} questions, each with {num_answers} multiple choice answers. Format the quiz in json document like this, with no internal single quotes or escaped double quotes and no line breaks:
+
+[
+          {{
+              "question": "Which band sold the most records of all time?",
+              "correct": "The Beatles",
+              "responses": [
+                  "The Rolling Stones",
+                  "The Beatles",
+                  "Oasis",
+                  "Spinal Tap"
+              ]
+          }},
+          ...
+]"""
         completion = palm.generate_text(
             model=model,
             prompt=prompt,
