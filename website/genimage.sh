b68145842385101e2b275ae05160d0cf726b029a
marcacohen <marcacohen@gmail.com>
2023-06-09 18:14:16 +0100

    add genimage.sh

--- website/genimage.sh
+++ website/genimage.sh
@@ -1,13 +1,17 @@
-cat >image-request.json >>!EOF
+#!/bin/sh
+
+TOPIC=$1
+
+cat >image-request.json <<!EOF
 {
     "instances": [
         {
-        "prompt": "A rock and roll star"
+        "prompt": "$TOPIC"
         }
     ],
     "parameters": {
         "sampleCount": 1,
-        "aspectRatio": "9:16",
+        "aspectRatio": "16:9",
         "negativePrompt": "blurry",
     }
 }
@@ -15,19 +19,18 @@ cat >image-request.json >>!EOF
 
 PROJECT_ID=cloud-llm-preview2
 
-curl -X POST \
+curl -s -X POST \
     -H "Authorization: Bearer $(gcloud auth print-access-token)" \
     -H "Content-Type: application/json; charset=utf-8" \
     -d @image-request.json \
-    "https://us-central1-aiplatform.googleapis.com/v1/projects/$PROJECT_ID/locations/us-central1/publishers/google/models/imagegeneration:predict" |
-    tee image-output.json
+    "https://us-central1-aiplatform.googleapis.com/v1/projects/$PROJECT_ID/locations/us-central1/publishers/google/models/imagegeneration:predict" \
+    | tee image-output.json
 
 IMAGE_NUM=0
 IMAGE_TYPE=$(cat image-output.json | jq .predictions[$IMAGE_NUM].mimeType)
 if [ '"image/png"' = "$IMAGE_TYPE" ] ; then
-    echo 'Image is a PNG. Lets decode it:'
     cat image-output.json | jq -r .predictions[$IMAGE_NUM].bytesBase64Encoded > t.base64
-    base64 -d t.base64 > "output/image-output-$IMAGE_NUM.JPG"
+    base64 -d -i t.base64 -o output/image-output-$IMAGE_NUM.jpg
 else
     echo NO: IMAGE_TYPE=$IMAGE_TYPE
 fi
