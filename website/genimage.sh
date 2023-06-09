a8601e6787ed2cd5889731c499ec6cdd875b5d11
marcacohen <marcacohen@gmail.com>
2023-06-09 18:25:14 +0100

    add genimage.sh tool

--- website/genimage.sh
+++ website/genimage.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 TOPIC=$1
+OUTPUT=$2
 
 cat >image-request.json <<!EOF
 {
     "instances": [
         {
-        "prompt": "$TOPIC"
+        "prompt": "photorealistic image about $TOPIC"
         }
     ],
     "parameters": {
         "sampleCount": 1,
-        "aspectRatio": "16:9",
         "negativePrompt": "blurry",
+        "aspectRatio": "16:9"
     }
 }
 !EOF
@@ -24,13 +25,15 @@ curl -s -X POST \
     -H "Content-Type: application/json; charset=utf-8" \
     -d @image-request.json \
     "https://us-central1-aiplatform.googleapis.com/v1/projects/$PROJECT_ID/locations/us-central1/publishers/google/models/imagegeneration:predict" \
-    | tee image-output.json
+    > image-output.json
 
 IMAGE_NUM=0
 IMAGE_TYPE=$(cat image-output.json | jq .predictions[$IMAGE_NUM].mimeType)
 if [ '"image/png"' = "$IMAGE_TYPE" ] ; then
     cat image-output.json | jq -r .predictions[$IMAGE_NUM].bytesBase64Encoded > t.base64
-    base64 -d -i t.base64 -o output/image-output-$IMAGE_NUM.jpg
+    base64 -d -i t.base64 -o $OUTPUT.jpg
+    gsutil cp $OUTPUT.jpg gs://quizrd-img/$OUTPUT.jpg
+    gsutil acl set public-read gs://quizrd-img/$OUTPUT.jpg
 else
     echo NO: IMAGE_TYPE=$IMAGE_TYPE
 fi
