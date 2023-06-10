a989603d634aaf5a36d67441f42bb21dd314b3c7
marcacohen <marcacohen@gmail.com>
2023-06-10 13:19:00 +0100

    make genimage reusable

--- website/genimage.sh
+++ website/genimage.sh
@@ -3,7 +3,7 @@
 TOPIC="$1"
 FILENAME=$(echo $2 | tr ' ' '-') 
 
-cat >image-request.json <<!EOF
+cat >request-$FILENAME.json <<!EOF
 {
     "instances": [
         {
@@ -23,18 +23,17 @@ PROJECT_ID=cloud-llm-preview2
 curl -s -X POST \
     -H "Authorization: Bearer $(gcloud auth print-access-token)" \
     -H "Content-Type: application/json; charset=utf-8" \
-    -d @image-request.json \
+    -d @request-$FILENAME.json \
     "https://us-central1-aiplatform.googleapis.com/v1/projects/$PROJECT_ID/locations/us-central1/publishers/google/models/imagegeneration:predict" \
-    > image-output.json
+    > output-$FILENAME.json
 
 IMAGE_NUM=0
-IMAGE_TYPE=$(cat image-output.json | jq .predictions[$IMAGE_NUM].mimeType)
+IMAGE_TYPE=$(cat output-$FILENAME.json | jq .predictions[$IMAGE_NUM].mimeType)
 if [ '"image/png"' = "$IMAGE_TYPE" ] ; then
-    cat image-output.json | jq -r .predictions[$IMAGE_NUM].bytesBase64Encoded > t.base64
-    base64 -d -i t.base64 -o $FILENAME.jpg
-    gsutil -q cp $FILENAME.jpg gs://quizrd-img/$FILENAME.jpg
-    gsutil -q acl set public-read gs://quizrd-img/$FILENAME.jpg
+    cat output-$FILENAME.json | jq -r .predictions[$IMAGE_NUM].bytesBase64Encoded > $FILENAME.base64
+    base64 -d -i $FILENAME.base64 -o $FILENAME.jpg
+    gsutil -q cp -a public-read $FILENAME.jpg gs://quizrd-img/$FILENAME.jpg
 else
     echo NO: IMAGE_TYPE=$IMAGE_TYPE
 fi
-rm -f image-request.json image-output.json t.base64 $FILENAME.jpg
+rm -f request-$FILENAME.json output-$FILENAME.json $FILENAME.base64 $FILENAME.jpg
