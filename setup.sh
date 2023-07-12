e195fdba75fc4a24edec2fd76a5ac703ec06dc65
marcacohen <marcacohen@gmail.com>
2023-07-12 10:40:53 +0100

    use echo -e in setup.sh to get newline chars

--- setup.sh
+++ setup.sh
@@ -1,28 +1,28 @@
-echo "=====\nEnabling cloud services...=====\n"
+echo -e "=====\nEnabling cloud services...=====\n"
 gcloud services enable run.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
 gcloud services enable firestore.googleapis.com
 gcloud firestore databases create --location nam3
 
-echo "=====\nSetting environment...=====\n"
+echo -e "=====\nSetting environment...=====\n"
 . scripts/env.sh
 
-echo "=====\nResetting firestore database...=====\n"
+echo -e "=====\nResetting firestore database...=====\n"
 cd content-api/data
 python3 seed_database.py unseed
 python3 seed_database.py seed marcacohen@gmail.com
 cd -
 
-echo "=====\nGenerating content API...=====\n"
+echo -e "=====\nGenerating content API...=====\n"
 npm install @openapitools/openapi-generator-cli -g
 scripts/regen_api.sh
 
-echo "=====\nBuilding and deploying content API...=====\n"
+echo -e "=====\nBuilding and deploying content API...=====\n"
 cd content-api
 ./deploy.sh
 cd -
 
-echo "=====\nBuilding and deploying website...=====\n"
+echo -e "=====\nBuilding and deploying website...=====\n"
 cd website
 pip install -r requirements.txt
 ./deploy.sh
