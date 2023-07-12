156584149dcbad89dc06aca2c7fa1f09a97a4ec5
marcacohen <marcacohen@gmail.com>
2023-07-12 20:33:37 +0100

    fix API_URL setting

--- scripts/env.sh
+++ scripts/env.sh
@@ -4,6 +4,6 @@ export GOOGLE_CLOUD_PROJECT=$(gcloud config get-value project)
 export PROJECT_ID=$GOOGLE_CLOUD_PROJECT
 export SESSION_BUCKET=${PROJECT_ID}-sessions
 export REDIRECT_URI=https://quizr.io/callback
-export API_URL=$(gcloud run services describe content-api --region=$REGION --project $PROD_PROJECT --format "value(status.url)")
+export API_URL=$(gcloud run services describe content-api --region=$REGION --project $PROJECT_ID --format "value(status.url)")
 
 export SITE_VARS="API_URL=$API_URL, SESSION_BUCKET=$SESSION_BUCKET, REDIRECT_URI=$REDIRECT_URI"
