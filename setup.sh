272faf1d2c1fbd77bc7bcc55b3d7c9b73ae65447
marcacohen <marcacohen@gmail.com>
2023-07-12 10:42:35 +0100

    tweak setup.sh

--- setup.sh
+++ setup.sh
@@ -1,28 +1,28 @@
-echo -e "=====\nEnabling cloud services...=====\n"
+echo -e "=====\nEnabling cloud services...\n=====\n"
 gcloud services enable run.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
 gcloud services enable firestore.googleapis.com
 gcloud firestore databases create --location nam3
 
-echo -e "=====\nSetting environment...=====\n"
+echo -e "=====\nSetting environment...\n=====\n"
 . scripts/env.sh
 
-echo -e "=====\nResetting firestore database...=====\n"
+echo -e "=====\nResetting firestore database...\n=====\n"
 cd content-api/data
 python3 seed_database.py unseed
 python3 seed_database.py seed marcacohen@gmail.com
 cd -
 
-echo -e "=====\nGenerating content API...=====\n"
+echo -e "=====\nGenerating content API...\n=====\n"
 npm install @openapitools/openapi-generator-cli -g
 scripts/regen_api.sh
 
-echo -e "=====\nBuilding and deploying content API...=====\n"
+echo -e "=====\nBuilding and deploying content API...\n=====\n"
 cd content-api
 ./deploy.sh
 cd -
 
-echo -e "=====\nBuilding and deploying website...=====\n"
+echo -e "=====\nBuilding and deploying website...\n=====\n"
 cd website
 pip install -r requirements.txt
 ./deploy.sh
