90d9aebe7d7c8fb52df5f598af7e206c57dafcf9
marcacohen <marcacohen@gmail.com>
2023-07-22 20:13:15 +0100

    remove extra test

--- quizgen/test.py
+++
@@ -1,6 +0,0 @@
-from quizgen.quizgen import Quizgen
-
-quiz = Quizgen("jeopardy")
-print(quiz)
-q = quiz.gen_quiz(topic="History", num_questions=3, num_answers=1, difficulty=5, temperature=.5)
-print(q)
