b12d79b15d35c9bf05a9a37195d3858ecd243ba6
marcacohen <marcacohen@gmail.com>
2023-07-23 11:37:57 +0100

    change name to pyquizrd

--- pyquizgen/README.md
+++
@@ -1,2 +0,0 @@
-pyquizgen is a Python package that creates trivia quizzes using static datasets and
-generative AI.
--- pyquizgen/gen.py
+++
@@ -1,6 +0,0 @@
-from pyquizgen.pyquizgen import Quizgen
-import pytest
-
-g = Quizgen("jeopardy")
-q = g.gen_quiz("History", 3, 1, 3, .5)
-print(q)
