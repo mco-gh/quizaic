44e940698ce5c8b4be67e7c7e8291ce02773cf6c
marcacohen <marcacohen@gmail.com>
2023-07-26 12:42:18 +0100

    remove unused emblem relics

--- installation-testing/terraform/modules/emblem-app/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  type        = string
-  description = "Project ID of project to deploy resources to."
-}
-
-variable "ops_project_id" {
-  type        = string
-  description = "Project ID of Emblem ops project."
-}
--- installation-testing/terraform/modules/ops/data.tf
+++
@@ -1,9 +0,0 @@
-data "google_project" "ops" {
-  project_id = var.project_id
-}
-
-data "google_pubsub_topic" "nightly" {
-  name     = var.nightly_topic_id
-  project  = var.project_id
-  provider = google
-}
