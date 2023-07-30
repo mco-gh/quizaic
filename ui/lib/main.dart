4b7482b07c89e87b7d8dedbe880beacb175190c7
marcacohen <marcacohen@gmail.com>
2023-07-30 10:33:06 +0100

    rename page files

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'home_page.dart';
+import 'home.dart';
 import 'app_state.dart';
 
 void main() async {
