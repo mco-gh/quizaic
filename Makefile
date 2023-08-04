a67f751104e1b73e111ea97d106f61d82370dbdf
marcacohen <marcacohen@gmail.com>
2023-08-04 10:31:41 +0100

    annotate env.sh

--- Makefile
+++ Makefile
@@ -1,5 +1,14 @@
+APP             := quizrd
 WEBSITE_VERSION := 0.0.1
 TAG             := ${REGION}-docker.pkg.dev/${PROJECT_ID}/quizrd/website:v${WEBSITE_VERSION}
+
+export REGION=us-central1
+export GOOGLE_CLOUD_PROJECT=$(gcloud config get-value project)
+export PROJECT_ID=$GOOGLE_CLOUD_PROJECT
+export SESSION_BUCKET=${PROJECT_ID}-sessions
+export REDIRECT_URI=https://quizrd.io/callback
+#export REDIRECT_URI=$(gcloud run services describe website --region=$REGION --project $PROJECT_ID --format "value(status.url)")/callback
+export API_URL=$(gcloud run services describe content-api --region=$REGION --project $PROJECT_ID --format "value(status.url)")
 
 # env vars need work
 
--- scripts/env.sh
+++ scripts/env.sh
@@ -1,10 +1,23 @@
+#
+# Project level environment variable settings. These may be reused in other scripts by
+# running ". scripts/env.sh".
+#
+# DEPENDENCY: You are expected to have set your project via "gcloud config set project blah".
+#
+
 export APP=quizrd
 export REGION=us-central1
 export GOOGLE_CLOUD_PROJECT=$(gcloud config get-value project)
 export PROJECT_ID=$GOOGLE_CLOUD_PROJECT
 export SESSION_BUCKET=${PROJECT_ID}-sessions
-export REDIRECT_URI=https://quizrd.io/callback
-#export REDIRECT_URI=$(gcloud run services describe website --region=$REGION --project $PROJECT_ID --format "value(status.url)")/callback
+
+# This line auto-detects the redirect URI for the quizrd content-api Cloud Run service.
 export API_URL=$(gcloud run services describe content-api --region=$REGION --project $PROJECT_ID --format "value(status.url)")
 
+# This line auto-detects the redirect URI for the quizrd website Cloud Run service,
+# but is now superceded by the next setting, which uses the quizrd app's domain name.
+#export REDIRECT_URI=$(gcloud run services describe website --region=$REGION --project $PROJECT_ID --format "value(status.url)")/callback
+export REDIRECT_URI=https://quizrd.io/callback
+
+# Variable settings passed into the website docker container when deploying to Cloud Run.
 export SITE_VARS="API_URL=$API_URL, SESSION_BUCKET=$SESSION_BUCKET, REDIRECT_URI=$REDIRECT_URI"
