9f773aef2134ce074815678ad6d5c4ceecd2fecd
marcacohen <marcacohen@gmail.com>
2023-08-07 14:22:49 +0100

    use Cloud Build for api deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,6 +15,11 @@ then
     . scripts/env.sh
     VERSION=$(cat website/version)
     TAG="${REGION}-docker.pkg.dev/${PROJECT_ID}/quizrd/website:v${VERSION}"
+    # I'd like to build this image using Cloud Build but because the build context
+    # needs to include the website and pyquizrd, the build must be run at the project
+    # level but Cloud Build won't let you specify an alternate path for the Dockerfile.
+    # Until I figure out a better solution, I'm using local docker build for this case.
+    #gcloud builds submit website --tag=$TAG
     docker build -f website/Dockerfile -t ${TAG} .
     docker push ${TAG}
     gcloud run deploy website --region ${REGION} --image=${TAG} \
@@ -25,8 +30,7 @@ then
     cd content-api
     VERSION=$(cat version)
     TAG="${REGION}-docker.pkg.dev/${PROJECT_ID}/quizrd/content_api:v${VERSION}"
-    docker build -t ${TAG} .
-    docker push ${TAG}
+    gcloud builds submit . --tag=$TAG
     gcloud run deploy content-api --region ${REGION} --image=${TAG} --allow-unauthenticated
     cd -
 else
