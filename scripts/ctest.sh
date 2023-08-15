c2871854f6d3182785b980bccd4f2f8cb5485b70
marcacohen <marcacohen@gmail.com>
2023-08-15 15:28:01 +0100

    add content-api test script and remove unused code in test.sh

---
+++ scripts/ctest.sh
@@ -0,0 +1,19 @@
+# ctest.sh - content API test script
+#
+USAGE="$0 path\n\nexport URL=url-to-content-api # default is http://localhost:8080\nexport METHOD=http-method     # default is GET"
+
+if [ "$#" != 1 ]; then
+    echo -e "${USAGE}"
+    exit 1
+fi
+if [ "$REDIRECT_URI" = "" ]; then
+    . ../scripts/env.sh
+fi
+if [ "$URL" = "" ]; then
+    export URL="localhost:8080"
+fi
+if [ "$METHOD" = "" ]; then
+    export METHOD=GET
+fi
+export ID_TOKEN=$(gcloud auth print-identity-token)
+curl -X "${METHOD}" -s "${URL}/${1}" -H "Authorization: Bearer $ID_TOKEN"
--- scripts/test.sh
+++ scripts/test.sh
@@ -31,14 +31,3 @@ else
     echo "Usage: $USAGE"
     exit 1
 fi
-
-# TODO: add curl testing of content-api
-#export URL="localhost:8080"
-#if [ "$REDIRECT_URI" = "" ]; then
-  #. ../scripts/env.sh
-#fi
-#if [ "$METHOD" = "" ]; then
-  #export METHOD=GET
-#fi
-#export ID_TOKEN=$(gcloud auth print-identity-token)
-#curl -X $METHOD -s $URL/$1 -H "Authorization: Bearer $ID_TOKEN"
