1df3b054ffb4b5e173a54025fb13f82115406ed4
marcacohen <marcacohen@gmail.com>
2023-08-15 15:15:34 +0100

    revise createQuiz page

--- ui/lib/views/create.dart
+++ ui/lib/views/create.dart
@@ -30,15 +30,15 @@ class CreateQuizFormState extends State<CreateQuizForm> {
 
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
+    // Build a Form widget using the _formKey created
+    const padding = 6.0;
     return Form(
-      key: _formKey,
-      child: SizedBox(
-        width: 600,
-        child: ListView(
-          children: [
+        key: _formKey,
+        child: SizedBox(
+          width: 600,
+          child: ListView(children: [
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              padding: const EdgeInsets.all(padding),
               child: TextFormField(
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
@@ -54,144 +54,123 @@ class CreateQuizFormState extends State<CreateQuizForm> {
               ),
             ),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              padding: const EdgeInsets.all(padding),
               child: TextFormField(
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: 'Quiz description',
+                  labelText: 'Number of Questions',
                 ),
                 // The validator receives the text that the user has entered.
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Missing quiz description';
+                    return 'Must be an integer greater than zero';
                   }
                   return null;
                 },
               ),
             ),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-              child: Row(children: [
-                SizedBox(
-                  width: 281,
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Number of Questions',
-                    ),
-                    // The validator receives the text that the user has entered.
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Must be an integer greater than zero';
-                      }
-                      return null;
-                    },
+              padding: const EdgeInsets.all(padding),
+              child: DropdownMenu<Difficulty>(
+                label: const Text('Difficulty Level'),
+                dropdownMenuEntries: [
+                  DropdownMenuEntry(
+                    label: 'Trivial',
+                    value: Difficulty.trivial,
                   ),
-                ),
-                const SizedBox(width: 22),
-                SizedBox(
-                  width: 281,
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Seconds to respond',
-                    ),
-                    // The validator receives the text that the user has entered.
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Must be an integer greater than zero';
-                      }
-                      return null;
-                    },
+                  DropdownMenuEntry(
+                    label: 'Easy',
+                    value: Difficulty.easy,
                   ),
-                ),
-              ]),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-              child: Row(
-                children: [
-                  DropdownMenu<Difficulty>(
-                    width: 181,
-                    label: const Text('Difficulty Level'),
-                    dropdownMenuEntries: [
-                      DropdownMenuEntry(
-                        label: 'Trivial',
-                        value: Difficulty.trivial,
-                      ),
-                      DropdownMenuEntry(
-                        label: 'Easy',
-                        value: Difficulty.easy,
-                      ),
-                      DropdownMenuEntry(
-                        label: 'Medium',
-                        value: Difficulty.medium,
-                      ),
-                      DropdownMenuEntry(
-                        label: 'Hard',
-                        value: Difficulty.hard,
-                      ),
-                      DropdownMenuEntry(
-                        label: 'Killer',
-                        value: Difficulty.killer,
-                      ),
-                    ],
+                  DropdownMenuEntry(
+                    label: 'Medium',
+                    value: Difficulty.medium,
                   ),
-                  const SizedBox(width: 20),
-                  DropdownMenu<YN>(
-                    width: 181,
-                    label: const Text('Randomize Questions'),
-                    dropdownMenuEntries: [
-                      DropdownMenuEntry(
-                        label: 'Yes',
-                        value: YN.yes,
-                      ),
-                      DropdownMenuEntry(
-                        label: 'No',
-                        value: YN.no,
-                      ),
-                    ],
+                  DropdownMenuEntry(
+                    label: 'Hard',
+                    value: Difficulty.hard,
                   ),
-                  const SizedBox(width: 20),
-                  DropdownMenu<YN>(
-                    width: 181,
-                    label: const Text('Randomize Answers'),
-                    dropdownMenuEntries: [
-                      DropdownMenuEntry(
-                        label: 'Yes',
-                        value: YN.yes,
-                      ),
-                      DropdownMenuEntry(
-                        label: 'No',
-                        value: YN.no,
-                      ),
-                    ],
+                  DropdownMenuEntry(
+                    label: 'Killer',
+                    value: Difficulty.killer,
                   ),
                 ],
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Validate returns true if the form is valid, or false otherwise.
-                  if (_formKey.currentState!.validate()) {
-                    // If the form is valid, display a snackbar. In the real world,
-                    // you'd often call a server or save the information in a database.
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Processing Data')),
-                    );
-                  }
-                },
-                child: const Text('Submit'),
+              padding: const EdgeInsets.all(padding),
+              child: Align(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, or false otherwise.
+                    if (_formKey.currentState!.validate()) {
+                      // If the form is valid, display a snackbar. In the real world,
+                      // you'd often call a server or save the information in a database.
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Creating quiz...')),
+                      );
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
               ),
             ),
-          ],
-        ),
-      ),
-    );
+          ]),
+        ));
   }
 }
+
+/*
+// The following form fields are quiz run-time, not create-time,
+// so they should be presented to the host when starting a quiz.
+const SizedBox(width: 22),
+SizedBox(
+width: 281,
+child: TextFormField(
+  decoration: const InputDecoration(
+    border: OutlineInputBorder(),
+    labelText: 'Seconds to respond',
+  ),
+  // The validator receives the text that the user has entered.
+  validator: (value) {
+    if (value == null || value.isEmpty) {
+      return 'Must be an integer greater than zero';
+    }
+    return null;
+  },
+),
+),
+const SizedBox(width: 20),
+DropdownMenu<YN>(
+  width: 181,
+  label: const Text('Randomize Questions'),
+  dropdownMenuEntries: [
+    DropdownMenuEntry(
+      label: 'Yes',
+      value: YN.yes,
+    ),
+    DropdownMenuEntry(
+      label: 'No',
+      value: YN.no,
+    ),
+  ],
+),
+const SizedBox(width: 20),
+DropdownMenu<YN>(
+  width: 181,
+  label: const Text('Randomize Answers'),
+  dropdownMenuEntries: [
+    DropdownMenuEntry(
+      label: 'Yes',
+      value: YN.yes,
+    ),
+    DropdownMenuEntry(
+      label: 'No',
+      value: YN.no,
+    ),
+  ],
+),
+*/
 
 class CreatePage extends StatelessWidget {
   @override
