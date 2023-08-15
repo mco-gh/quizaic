47e41d8d2d9f4cd328bc0d28d899b0b4138b57b1
Mete Atamel <atamel@google.com>
2023-08-15 12:51:45 +0100

    Cleanup of quizgen and quizeval #48

--- pyquizrd/generators/quiz/palm/quizeval.py
+++ pyquizrd/generators/quiz/palm/quizeval.py
@@ -9,30 +9,30 @@ from pyquizrd.generators.quiz.basequizeval import BaseQuizeval
 from pyquizrd.generators.quiz.palm.quizgen import Quizgen
 
 MODEL = "text-bison"
-
-EVAL_PROMPT_FILE = "prompt_eval4.txt"
-EVAL_TEMPERATURE = 0.3
-EVAL_TOP_K = 40
-EVAL_TOP_P = 1
+PROMPT_FILE = "eval_4.txt"
+TEMPERATURE = 0.3
+TOP_K = 40
+TOP_P = 1
 
 
 class Quizeval(BaseQuizeval):
 
     def __init__(self):
         super().__init__()
-        file_path = os.path.join(os.path.dirname(__file__), "prompts/" + EVAL_PROMPT_FILE)
+        file_path = os.path.join(os.path.dirname(__file__), "prompts/" + PROMPT_FILE)
         with open(file_path, encoding='utf-8') as fp:
-            self.prompt_eval = fp.read()
+            self.prompt_template = fp.read()
 
     def __str__(self):
         return "palm quiz evaluator"
 
     @staticmethod
-    def predict_llm(model, temp, tokens, top_p, top_k, content, tuned_model=""):
+    def predict_llm(model, prompt, temperature, max_output_tokens, top_p, top_k, tuned_model=""):
         model = TextGenerationModel.from_pretrained(model)
         if tuned_model:
             model = model.get_tuned_model(tuned_model)
-        response = model.predict(content, temperature=temp, max_output_tokens=tokens, top_k=top_k, top_p=top_p)
+        response = model.predict(prompt, temperature=temperature, max_output_tokens=max_output_tokens, top_k=top_k,
+                                 top_p=top_p)
         return response.text
 
     def eval_quiz(self, quiz, topic, num_questions, num_answers, shortcircuit_validity=True):
@@ -45,26 +45,26 @@ class Quizeval(BaseQuizeval):
         for item in quiz_eval:
             item.pop("correct")
 
-        prompt_eval = self.prompt_eval.format(quiz=json.dumps(quiz_eval, indent=4), topic=topic)
-        eval = self.predict_llm(MODEL, EVAL_TEMPERATURE, 1024, EVAL_TOP_P, EVAL_TOP_K, prompt_eval)
+        prompt = self.prompt_template.format(quiz=json.dumps(quiz_eval, indent=4), topic=topic)
+        prediction = self.predict_llm(MODEL, prompt, TEMPERATURE, 1024, TOP_P, TOP_K)
         try:
-            if eval == "":  # Happens when a question is not safe according to LLM
+            if prediction == "":  # Happens when a question is not safe according to LLM
                 validity["valid_quiz"] = False
                 validity["unknown_questions"] = num_questions
                 validity["details"].append("Invalid #4: Cannot evaluate quiz due to unsafe questions")
                 return self.get_validity_compact(validity)
 
             # Sometimes the model returns invalid JSON with a trailing comma, remove it.
-            if eval[-3:] == ",\n]":
-                eval = eval[:-3] + "\n]"
+            if prediction[-3:] == ",\n]":
+                prediction = prediction[:-3] + "\n]"
 
-            eval_json = json.loads(eval)
+            evaluation = json.loads(prediction)
         except ValueError as e:
-            print(f'eval:"{eval}"')
+            print(f'prediction:"{prediction}"')
             raise ValueError("An exception occurred during JSON parsing", e)
 
         # [{"on_topic": true, "correct": ["George Washington"], "incorrect": ["Benjamin Franklin", "Thomas Jefferson"]},
-        for index, item in enumerate(eval_json):
+        for index, item in enumerate(evaluation):
             question = quiz[index]['question']
 
             if not item["on_topic"]:
@@ -85,7 +85,7 @@ class Quizeval(BaseQuizeval):
                 if shortcircuit_validity:
                     return self.get_validity_compact(validity)
 
-            # Only add for prompt_eval3.txt
+            # Only add for eval_3.txt
             # responses = quiz_eval[index]["responses"]
             # if item["correct"] in responses:
             #     responses.remove(item["correct"])
@@ -106,7 +106,7 @@ class Quizeval(BaseQuizeval):
 
 
 if __name__ == "__main__":
-    eval = Quizeval()
+    evaluator = Quizeval()
     print(f'eval:{eval}')
 
     gen = Quizgen()
@@ -116,5 +116,5 @@ if __name__ == "__main__":
     quiz = gen.gen_quiz("science", num_questions, num_answers)
     print(json.dumps(quiz, indent=4))
 
-    validity = eval.eval_quiz(quiz, topic, num_questions, num_answers)
+    validity = evaluator.eval_quiz(quiz, topic, num_questions, num_answers)
     print(json.dumps(validity, indent=4))
--- pyquizrd/generators/quiz/palm/quizgen.py
+++ pyquizrd/generators/quiz/palm/quizgen.py
@@ -10,24 +10,22 @@ sys.path.append("../../../../") # Needed for the main method to work in this cla
 from pyquizrd.generators.quiz.basequizgen import BaseQuizgen
 
 MODEL = "text-bison"
-
-DEFAULT_PROMPT_GEN_FILE = "prompt_gen2.txt"
+PROMPT_FILE = "gen_2.txt"
+TEMPERATURE = 0.5
+TOP_P = 0.8
+TOP_K = 40
 
 class Quizgen(BaseQuizgen):
 
     def __init__(self, config=None):
-        prompt_gen_file = DEFAULT_PROMPT_GEN_FILE
-
-        if config:
-            prompt_gen_file = config.get("prompt_gen_file", DEFAULT_PROMPT_GEN_FILE)
-            # This doesn't seem to be needed
-            # vertexai.init(project=project_id, location=region)
+        # This doesn't seem to be needed
+        # vertexai.init(project=project_id, location=region)
 
         self.topics = set()
 
-        file_path = os.path.join(os.path.dirname(__file__), "prompts/" + prompt_gen_file)
+        file_path = os.path.join(os.path.dirname(__file__), "prompts/" + PROMPT_FILE)
         with open(file_path, encoding='utf-8') as fp:
-            self.prompt_gen = fp.read()
+            self.prompt_template = fp.read()
 
     def __str__(self):
         return "palm quiz generator"
@@ -42,11 +40,12 @@ class Quizgen(BaseQuizgen):
         return ["free-form", "multiple-choice"]
 
     @staticmethod
-    def predict_llm(model, temp, tokens, top_p, top_k, content, tuned_model=""):
+    def predict_llm(model, prompt, temperature, max_output_tokens, top_p, top_k, tuned_model=""):
         model = TextGenerationModel.from_pretrained(model)
         if tuned_model:
             model = model.get_tuned_model(tuned_model)
-        response = model.predict(content, temperature=temp, max_output_tokens=tokens, top_k=top_k, top_p=top_p)
+        response = model.predict(prompt, temperature=temperature, max_output_tokens=max_output_tokens, top_k=top_k,
+                                 top_p=top_p)
         return response.text
 
     @staticmethod
@@ -66,7 +65,6 @@ class Quizgen(BaseQuizgen):
     def load_quiz(quiz_file):
         file = open(os.path.join(os.path.dirname(__file__), "quizzes/" + quiz_file))
         quiz = json.load(file)
-        #print(json.dumps(quiz, indent=4))
 
         topic = re.search(r"_(.*)\.json", quiz_file).group(1)
         num_questions = len(quiz)
@@ -74,13 +72,11 @@ class Quizgen(BaseQuizgen):
 
         return quiz, topic, num_questions, num_answers
 
-    def gen_quiz(self, topic, num_questions, num_answers, difficulty=3, temperature=.5):
-        prompt = self.prompt_gen.format(topic=topic,
-            num_questions=num_questions,
-            num_answers=num_answers,
-            difficulty=self.get_difficulty_word(difficulty))
-        quiz = self.predict_llm(MODEL, temperature, 1024, 0.8, 40, prompt)
-        quiz = json.loads(quiz)
+    def gen_quiz(self, topic, num_questions, num_answers, difficulty=3, temperature=TEMPERATURE):
+        prompt = self.prompt_template.format(topic=topic, num_questions=num_questions, num_answers=num_answers,
+                                             difficulty=self.get_difficulty_word(difficulty))
+        prediction = self.predict_llm(MODEL, prompt, temperature, 1024, TOP_P, TOP_K)
+        quiz = json.loads(prediction)
         # Make sure the correct answer appears randomly in responses
         for i in quiz:
             random.shuffle(i["responses"])
