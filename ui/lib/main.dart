f51a61bbd4faeb13f308e2e7eec3a353aab5cf0c
marcacohen <marcacohen@gmail.com>
2023-09-01 23:28:27 -0400

    use url path strategy (avoid hashes in url links)

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -2,9 +2,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'views/home.dart';
 import 'models/state.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'config/firebase_options.dart';
 
 void main() async {
@@ -12,6 +13,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  usePathUrlStrategy();
   runApp(MyApp());
 }
 
