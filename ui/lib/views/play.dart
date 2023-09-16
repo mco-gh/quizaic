420301e54205c051698053521e2e63851e05bdea
marcacohen <marcacohen@gmail.com>
2023-09-16 21:48:23 +0100

    add new assets and files for play pages

--- ui/lib/views/play.dart
+++ ui/lib/views/play.dart
@@ -1,17 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:quizaic/constants.dart';
 
 class PlayPage extends StatelessWidget {
-  final String? quizId;
-
   @override
   Widget build(BuildContext context) {
-    //var theme = Theme.of(context);
-    //var appState = context.watch<AppState>();
-
-    return Center(
-      child: Text('Playing quiz $quizId'),
+    return Scaffold(
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(flex: 2), //2/6
+                  Text(
+                    "Let's Play!",
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  Text("Enter your name"),
+                  Spacer(), // 1/6
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFF1C2341),
+                      hintText: "Full Name",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                    ),
+                  ),
+                  Spacer(), // 1/6
+                  InkWell(
+                    onTap: () => GoRouter.of(context).go('/play'),
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(kDefaultPadding * 0.75), // 15
+                      decoration: BoxDecoration(
+                        gradient: kPrimaryGradient,
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                      child: Text(
+                        "Lets Start Quiz",
+                        style: Theme.of(context)
+                            .textTheme
+                            .labelLarge
+                            ?.copyWith(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  Spacer(flex: 2), // it will take 2/6 spaces
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
-
-  PlayPage({this.quizId});
 }
---
+++ ui/lib/views/quiz.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class QuizPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    //var theme = Theme.of(context);
+    //var appState = context.watch<AppState>();
+
+    return Center(
+      child: Text('Playing quiz'),
+    );
+  }
+
+  QuizPage();
+}
