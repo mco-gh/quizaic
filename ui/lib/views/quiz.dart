bf0b039a6ebbdeb5050770e6216f54b18b660044
marcacohen <marcacohen@gmail.com>
2024-04-17 22:30:21 +0200

    fix horizontal scrolling on quiz play page

--- ui/lib/views/quiz.dart
+++ ui/lib/views/quiz.dart
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:quizaic/models/state.dart';
@@ -254,8 +255,11 @@ class QuizPage extends StatelessWidget {
         }
         return SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          child: Column(
-            children: widgets,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Column(
+              children: widgets,
+            ),
           ),
         );
       },
