79c47066d4099e4cfdaa6d80dfe21157b331035b
marcacohen <marcacohen@gmail.com>
2024-05-02 16:53:26 +0100

    make static gens use static quiz image

--- api/resources/methods.py
+++ api/resources/methods.py
@@ -272,17 +272,22 @@ def patch(resource_kind, id, representation):
             representation["qAndA"] = json.dumps(quiz)
 
         if regen_image:
-            try:
-                print("generating a new quiz image...")
-                filename = id + ".png"
-                file_url = ImageGen.generate_and_upload_image(
-                    topic, filename, IMAGES_BUCKET
-                )
-                print(f"file_url: {file_url}")
-                representation["imageUrl"] = file_url
-            except:
-                print(f"imagegen failed, using default image")
+            selected_gen = generator.lower()
+            if selected_gen == "opentrivia" or selected_gen == "jeopardy":
+                print(f"static generators use default image")
                 representation["imageUrl"] = "/assets/assets/images/quizaic_logo.png"
+            else:
+                try:
+                    print("generating a new quiz image...")
+                    filename = id + ".png"
+                    file_url = ImageGen.generate_and_upload_image(
+                        topic, filename, IMAGES_BUCKET
+                    )
+                    print(f"file_url: {file_url}")
+                    representation["imageUrl"] = file_url
+                except:
+                    print(f"imagegen failed, using default image")
+                    representation["imageUrl"] = "/assets/assets/images/quizaic_logo.png"
 
 
     if resource_kind == "results":
